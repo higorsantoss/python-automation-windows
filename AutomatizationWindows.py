7c68fd8750b932fcfeadd0196b72d432beebff6d
Higor Santos <higorsantoss@gmail.com>
2022-03-12 01:11:49 -0500

    Version 1

---
+++ AutomatizationWindows.py
@@ -0,0 +1,62 @@
+# %% [markdown]
+# Automation Windows, Open Microsoft Edge and download a txt file
+
+# %%
+import pyperclip
+import time
+import pyautogui
+import shutil
+import os
+import logging
+from os.path import exists, abspath
+from pathlib import Path
+
+
+# Wait 1 second between each command pyautogui
+pyautogui.PAUSE = 1  
+
+# Open browser Windowshttps://file-examples.com/index.php/sample-documents-download/sample-xls-download/
+
+pyautogui.hotkey('win')
+pyautogui.write('edge')
+pyautogui.press('enter')
+
+pyperclip.copy('https://filesamples.com/formats/txt')
+pyautogui.hotkey('ctrl', 'v')
+pyautogui.press('enter')
+
+# Wait 5 seconds until get position x and y of mouse after that you can decrease the time to 2 seconds
+time.sleep(3)
+
+# print a position of mouse after time.sleep(3)
+#pyautogui.position()
+
+# if you want 2 or mores clicks add the parameter clicks=2
+pyautogui.click(x=3746, y=607, clicks=1) 
+
+# %%
+# Get path or directory windows 
+downloads_path = str(Path.home() / "Downloads")
+downloads_path += "\\"
+
+# Get classpath of project 
+project_root_dir = abspath(os.curdir)
+
+file = "sample3.txt"
+file_complet = downloads_path + file
+
+# Verify if the file exists in the directory
+file_exists_downloads = exists(file_complet)
+file_exists_project_root = exists(project_root_dir+ "\\" + file)
+
+if(file_exists_project_root):
+    logging.info('file already copied to the project directory')  
+elif(file_exists_downloads):
+    # Copy file to another directory
+    shutil.copy(file_complet, project_root_dir)
+    logging.info('File copied successfully')  
+else:
+    logging.error('Could not copy the file. File not found.')
+    
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python-automation-windows
+
+module that uses interaction with windows (keyboard / mouse) and can download a file from a website and check if it was created in the directory
